a980b426b40573e8684e6183c184725d751a3164
Prasad Konka <prasad.konka@nih.gov>
2014-10-02 22:58:09 -0400

    LDAP config changes

--- cagrid-authentication/cagrid-authentication-soap-client/src/main/java/org/cagrid/authentication/soapclient/AuthenticationServiceSoapClientFactory.java
+++ cagrid-authentication/cagrid-authentication-soap-client/src/main/java/org/cagrid/authentication/soapclient/AuthenticationServiceSoapClientFactory.java
@@ -1,10 +1,20 @@
 package org.cagrid.authentication.soapclient;
 
 import org.apache.cxf.Bus;
-import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.cagrid.authentication.wsrf.service.AuthenticationServiceService;
 import org.cagrid.gaards.authentication.AuthenticationServicePortType;
+import org.apache.cxf.configuration.security.KeyStoreType;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.configuration.Configurer;
+import org.apache.cxf.configuration.security.KeyManagersType;
+import org.apache.cxf.configuration.security.KeyStoreType;
+import org.cagrid.core.common.security.SSLConfigurer;
+import org.cagrid.core.common.security.X509Credential;
+
+import javax.net.ssl.KeyManager;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class AuthenticationServiceSoapClientFactory {
 
@@ -12,12 +22,66 @@ public class AuthenticationServiceSoapClientFactory {
         SpringBusFactory bf = new SpringBusFactory();
         Bus bus = bf.createBus();
 
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(AuthenticationServiceService.class);
+        cf.setBus(bus);
+        AuthenticationServicePortType authPort = cf.create(AuthenticationServicePortType.class);
+        return authPort;
+    }
+    
+    public static AuthenticationServicePortType createSoapClient(String url, KeyStoreType truststore, KeyManagersType keyManager)
+            throws GeneralSecurityException, IOException {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKeystore(keyManager);
+        bus.setExtension(sslConf, Configurer.class);
 
         JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
         cf.setAddress(url);
         cf.setServiceClass(AuthenticationServiceService.class);
         cf.setBus(bus);
-        AuthenticationServicePortType gridGrouperPort = cf.create(AuthenticationServicePortType.class);
-        return gridGrouperPort;
+        AuthenticationServicePortType authPort = cf.create(AuthenticationServicePortType.class);
+        return authPort;
+    }
+
+    public static AuthenticationServicePortType createSoapClient(String url, KeyStoreType truststore, X509Credential cred)
+            throws GeneralSecurityException, IOException {
+
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setCredential(cred);
+        bus.setExtension(sslConf, Configurer.class);
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(AuthenticationServiceService.class);
+        cf.setBus(bus);
+        AuthenticationServicePortType authPort = cf.create(AuthenticationServicePortType.class);
+        return authPort;
+    }
+
+    public static AuthenticationServicePortType createSoapClient(String url, KeyStoreType truststore, KeyManager keyManager)
+            throws GeneralSecurityException, IOException {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKm(new KeyManager[] { keyManager });
+        bus.setExtension(sslConf, Configurer.class);
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(AuthenticationServiceService.class);
+        cf.setBus(bus);
+        AuthenticationServicePortType authPort = cf.create(AuthenticationServicePortType.class);
+        return authPort;
     }
 }
--- cagrid-authentication/cagrid-authentication-soap-client/src/test/java/org/cagrid/authentication/soapclient/AuthenticationServiceClientBase.java
+++ cagrid-authentication/cagrid-authentication-soap-client/src/test/java/org/cagrid/authentication/soapclient/AuthenticationServiceClientBase.java
@@ -1,14 +1,21 @@
 package org.cagrid.authentication.soapclient;
 
 import org.cagrid.gaards.authentication.AuthenticationServicePortType;
+import javax.net.ssl.KeyManager;
+
+import org.apache.cxf.configuration.security.KeyStoreType;
 
 public abstract class AuthenticationServiceClientBase {
 
-	public final static String LOCAL_URL = "https://localhost:7742/authentication";
+	public final static String LOCAL_URL = "https://localhost:4443/authentication";
 
 	protected AuthenticationServicePortType authentication;
 
 	AuthenticationServiceClientBase(String url) throws Exception {
-        authentication = AuthenticationServiceSoapClientFactory.createSoapClient(url);
+		KeyStoreType truststore = new KeyStoreType();
+		truststore.setUrl(getClass().getClassLoader().getResource("truststore.jks").toString());
+		truststore.setType("JKS");
+		truststore.setPassword("changeit");
+		authentication = AuthenticationServiceSoapClientFactory.createSoapClient(LOCAL_URL, truststore, (KeyManager) null);
 	}
 }
